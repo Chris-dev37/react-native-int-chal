4c4d4099af7ec06bfabe772e1e06cd15323fd518
c23-day <christopher4.day@live.uwe.ac.uk>
2021-07-03 18:44:22 +0100

    weather data extracted but not implemented

--- src/WeatherScreen.tsx
+++ src/WeatherScreen.tsx
@@ -3,46 +3,55 @@ import { View, Text } from 'react-native';
 
 const WeatherScreen = () => {
 
-  const tempArray = [];
+  let weatherArray = [];
+
+  const compareTime = (data) => {
+    let i = 0;
+
+
+    while (i < data["list"].length) {
+
+      let compareDate = new Date();
+      compareDate.setDate(compareDate.getDate()  + i);
+      compareDate.setHours(13, 0, 0);
+      compareDate.setMilliseconds(0);
+
+      // console.log(i);
+      // let date = new Date(data["list"][i].dt * 1000)
+      // console.log(date);
+      
+      console.log(compareDate.getTime());
+      console.log(data["list"][i].dt * 1000);
+
+
+      if ((data["list"][i].dt * 1000) === compareDate.getTime()) {
+        console.log(data["list"][i].dt);
+        console.log(data["list"][i]["main"]["temp"]);
+        weatherArray.push(data["list"][i]["main"]["temp"]);
+
+      }
+      i++;
+      //console.log(weatherArray);
+    }
+  }
 
   fetch("http://api.openweathermap.org/data/2.5/forecast?zip=GL51,GB&appid=12dd7ca9ea3ee83b8905ec822844bcd1")
     .then(response => response.json())
     .then((data) => {
-      //let weatherData = data
       //console.log(data["list"][0].dt);
-
-
-      let nextDay = new Date();
-      nextDay.setDate(nextDay.getDate() + 1);
-      nextDay.setHours(12, 0, 0)
-      nextDay.getTime() / 1000;
-
-      let i = 0;
-      while (i < 7) {
-        //console.log(nextDay);
-        if ((data["list"][i].dt) === nextDay) {
-          tempArray.push(data["list"][i]["main"]["temp"]);
-
-        }
-        i++;
-        console.log(tempArray);
-      }
-
-
+      compareTime(data);
     })
     .catch((error) => {
       console.error(error)
     })
-  // console.log(tempArray);
-
 
   return (
     <View style={{ flex: 1, backgroundColor: '#ff4081' }}>
       <Text>Page 2</Text>
       <Text>Temperature this week will be ...</Text>
-      {/* <ul>
-        {tempArray}
-      </ul> */}
+      <ul>
+        {weatherArray}
+      </ul>
     </View>
   )
 };
